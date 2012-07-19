795c9fa67e53a07e36f8cdd1d7628f01baf15069
Maxim Gavrilov <maxim.gavrilov@gmail.com>
2012-07-19 13:52:17 +0400

    locker

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
-module.exports = (function () {
-    function Lock() {
+"use strict"
+var assert = require('assert');
 
-    }
+var Lock = (function () {
+    function Lock(module, keys) {
+        this.module = module;
+        this.keys = keys;
+    };
+
+    Lock.prototype.release = function() {
+        this.module.release(this);
+    };
 
     return Lock;
+})();
+
+module.exports = (function () {
+    function LockModule() {
+        this.deffered = {};
+    }
+
+    function _iter_keys(keys, fn, callback) {
+        var keys = keys.concat();
+        function iter() {
+            var key = keys.shift();
+            if (key) 
+                fn(key, iter);
+            else 
+                callback();
+        }
+        iter();
+    }
+
+    LockModule.prototype.acquire = function(keys, callback) {
+        if (!Array.isArray(keys))
+            keys = [keys];
+
+        var deffered = this.deffered;
+        var lock = new Lock(this, keys);
+
+        _iter_keys(lock.keys.sort(), function (key, callback) {
+            if (deffered[key]) {
+                deffered[key].push(callback);
+            } else {
+                deffered[key] = [];
+                callback();
+            }            
+        }, function (err) {
+            if (err) return callback(err);
+            callback(null, lock);
+        });
+    };
+
+    LockModule.prototype.release = function(lock) {
+        var deffered = this.deffered;
+
+        assert.ok(lock.keys.every(function (k) { return deffered[k];}));
+
+        lock.keys.sort().reverse().forEach(function (key) {
+            var def_fn = deffered[key].shift();
+            if (def_fn) {
+                def_fn();
+            } else {
+                delete deffered[key];
+            }
+        });
+    };
+    return LockModule;
 })();
--- test/lock.js
+++ test/lock.js
@@ -1,38 +1,219 @@
-var assert = require('assert');
+"use strict"
+// NodeUnit test suite
 var Lock = require('../');
+var nextTick = process.nextTick;
 
 exports.lock_test = {
+    utils : function (test) {
+        test.ok([]);
+        test.ok([1, 2, 3]);
+        test.ok(undefined ? false : true);
+        test.ok(!Array.isArray(undefined));
+        test.ok(![false, false, false].some(function (v) {return v;}));
+        test.deepEqual([2, 3], [1, 2, 3, 4, 5].filter(function (v) {
+            return [2, 3].indexOf(v) >= 0;
+        }));
+        test.done();
+    },
+
     "one lock": function (test) {
         var lock = new Lock();
-        test.ok(true);
-        test.done();
+        lock.acquire("res1", function (err, lock) {
+            if (err) return test.done(err);
+
+            lock.release();
+            test.done();
+        });
+    },
+
+    "one internal lock": function (test) {
+        test.expect(1);
+
+        var lock = new Lock();
+        lock.acquire("res", function (err, lock1) {
+            if (err) return test.done(err);
+            var lock1_released = false;
+
+            lock.acquire("res", function (err, lock2) {
+                if (err) return test.done(err);
+
+                test.equals(lock1_released, true);
+                lock2.release();
+                test.done();
+            });
+
+            setTimeout(function () {
+                lock1_released = true;
+                lock1.release();                
+            }, 10);
+        });
+    },
+
+    "two internal locks": function (test) {
+        test.expect(3);
+
+        var lock = new Lock();
+        lock.acquire("res", function (err, lock1) {
+            if (err) return test.done(err);
+            var lock1_released = false;
+
+            lock.acquire("res", function (err, lock2) {                
+                if (err) return test.done(err);
+
+                test.ok(lock1_released);
+
+                var lock2_released = false;
+
+                lock.acquire("res", function (err, lock3) {
+                    if (err) return test.done(err);
+
+                    test.ok(lock1_released);
+                    test.ok(lock2_released);
+                    lock3.release();
+                    test.done();
+                })
+
+                setTimeout(function () {
+                    lock2_released = true;
+                    lock2.release();                
+                }, 10);
+            });
+
+            setTimeout(function () {
+                lock1_released = true;
+                lock1.release();                
+            }, 10);
+        });
+    },
+
+    "one internal multilock": function (test) {
+        test.expect(1);
+
+        var lock = new Lock();
+        lock.acquire(["res1", "res2"], function (err, lock1) {
+            var lock1_released = false;
+
+            lock.acquire("res1", function (err, lock2) {
+                if (err) return test.done(err);
+
+                test.ok(lock1_released);
+                test.done();
+            });
+
+            setTimeout(function () {
+                lock1_released = true;
+                lock1.release();
+            }, 10);
+        });
+    },
+
+    "one internal opposite inner multilock": function (test) {
+        test.expect(1);
+
+        var lock = new Lock();
+        lock.acquire(["res1", "res2"], function (err, lock1) {
+            var lock1_released = false;
+
+            lock.acquire(["res2", "res1"], function (err, lock2) {
+                if (err) return test.done(err);
+
+                test.ok(lock1_released);
+                test.done();
+            });
+
+            setTimeout(function () {
+                lock1_released = true;
+                lock1.release();
+            }, 10);
+        });
+    },
+
+    "two locks – both locks – single lock": function (test) {
+        test.expect(3);
+
+        var lock = new Lock();
+        lock.acquire("res1", function (err, lock1) {
+            if (err) return test.done(err);
+
+            var lock1_released = false;
+            lock.acquire("res2", function (err, lock2) {
+                if (err) return test.done(err);
+
+                var lock2_released = false;
+                var lock3_released = false;
+
+                lock.acquire(["res1", "res2"], function (err, lock3) {
+                    if (err) return test.done(err);
+
+                    lock3_released = true;
+                    lock3.release();
+                });
+
+                setTimeout(function () {
+                    lock1_released = true;
+                    lock1.release();
+
+                    lock2_released = true;
+                    lock2.release();
+
+                    setTimeout(function () {
+                        lock.acquire("res1", function (err, lock_) {
+                            test.ok(lock1_released);
+                            test.ok(lock2_released);
+                            test.ok(lock3_released);
+
+                            lock_.release();
+
+                            test.done();
+                        });
+                    }, 10);
+                }, 10);
+
+            });
+        });
+    },
+
+    "two concurent opposite locks": function (test) {
+        test.expect(2);
+
+        var lock = new Lock();
+        lock.acquire("res1", function (err, lock1) {
+            if (err) return test.done(err);
+
+            var lock1_released = false;
+            var lock2_released = false;
+            var lock3_released = false;
+
+            lock.acquire(["res1", "res2"], function (err, lock2) {
+                if (err) return test.done(err);
+
+                test.ok(lock1_released);
+
+                var _lock3_released = lock3_released;
+                lock2_released = true;
+                lock2.release();
+
+                if (lock2_released && _lock3_released) {
+                    test.done();
+                }
+            });
+
+            lock.acquire(["res2", "res1"], function (err, lock3) {
+                if (err) return test.done(err);
+
+                test.ok(lock1_released);
+
+                var _lock2_released = lock2_released;
+                lock3_released = true;
+                lock3.release();
+
+                if (_lock2_released && lock3_released) {
+                    test.done();
+                }
+            });
+
+            lock1_released = true;
+            lock1.release();
+        });
     }
 }
-
-// exports.hard = function () {
-//     var s = 'Assert from {"type":"equal","ok":false,"found":1,"wanted":2,'
-//         + '"stack":[],"id":"b7ddcd4c409de8799542a74d1a04689b",'
-//         + '"browser":"chrome/6.0"}'
-//     ;
-//     var s_ = wordwrap.hard(80)(s);
-    
-//     var lines = s_.split('\n');
-//     assert.equal(lines.length, 2);
-//     assert.ok(lines[0].length < 80);
-//     assert.ok(lines[1].length < 80);
-    
-//     assert.equal(s, s_.replace(/\n/g, ''));
-// };
-
-// exports.break = function () {
-//     var s = new Array(55+1).join('a');
-//     var s_ = wordwrap.hard(20)(s);
-    
-//     var lines = s_.split('\n');
-//     assert.equal(lines.length, 3);
-//     assert.ok(lines[0].length === 20);
-//     assert.ok(lines[1].length === 20);
-//     assert.ok(lines[2].length === 15);
-    
-//     assert.equal(s, s_.replace(/\n/g, ''));
-// };
